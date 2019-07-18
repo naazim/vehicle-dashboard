f05513a6a8b44312149c1a2c74ae2cf0b8d0517c
Naasim Shareef <naazim4u@gmail.com>
2019-07-18 14:52:18 +0200

    Initial commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {style} from './index.scss';
+import './_header.scss';
 
 const Header = ({}) => {
 
---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Gauge from '../components/Gauge';
+import '../scss/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Gauge />
+    </div>
+  );
+}
+
+export default App;
