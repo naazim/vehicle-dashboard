b2d91dc9f5e701ec60d976dde01d1d0b14e5d188
Naasim Shareef <naazim4u@gmail.com>
2019-07-23 16:27:13 +0200

    Speedometer

---
+++ src/components/Map/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Map = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Map.propTypes = {
+
+};
+
+export default Map;
