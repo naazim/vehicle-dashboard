0ec71a93a1878bb39e60230a9c9a16be66b161f5
Naasim Shareef <naazim4u@gmail.com>
2019-08-02 19:03:34 +0200

    Nav header UI

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Fleets } from '../../components/Fleets';
 import { VehicleDetail } from '../../components/VehicleDetail';
+import { Button } from '../../components/Button';
+import FleetLogo from '../../assets/fleet-logo.svg';
 
 const API_FLEETS_URL = '/fleet';
 
@@ -47,6 +49,12 @@ class Nav extends Component {
     const { fleets, fleetVehicles, vehicle: vehicleData } = this.state;
     return (
       <nav className="fl-nav">
+        <div className="fl-nav__header">
+          <img className="fl-nav__logo" src={FleetLogo} alt="Fleet logo" />
+          <Button type="button" className="fl-hamburger btn-icon">
+            ☰
+          </Button>
+        </div>
         <Fleets
           data={fleets}
           fleetVehicles={fleetVehicles}
