00cdd7ed1ed99009a0b263eef9138ab906bd07e4
Naasim Shareef <naazim4u@gmail.com>
2019-08-07 16:53:07 +0200

    Theme updates

--- src/components/ThemeSwitcher/index.js
+++ src/components/ThemeSwitcher/index.js
@@ -2,21 +2,17 @@ import React from 'react';
 import clsx from 'clsx';
 import { ReactComponent as SwitchIcon } from '../../assets/switch.svg';
 
-const ThemeSwitcher = ({ toggleTheme, currentTheme }) => {
-  const theme = currentTheme === 'dark' ? 'light' : 'dark';
-
-  return (
-    <button
-      onClick={toggleTheme}
-      className={clsx(
-        'fl-theme-switcher btn-icon',
-        `fl-theme-switcher--${theme}`
-      )}
-    >
-      <SwitchIcon className="fl-theme-switcher__icon" />
-      {theme}
-    </button>
-  );
-};
+const ThemeSwitcher = ({ toggleTheme, currentTheme }) => (
+  <button
+    onClick={toggleTheme}
+    className={clsx(
+      'fl-theme-switcher btn-icon',
+      `fl-theme-switcher--${currentTheme}`
+    )}
+  >
+    <SwitchIcon className="fl-theme-switcher__icon" />
+    {currentTheme}
+  </button>
+);
 
 export { ThemeSwitcher };
