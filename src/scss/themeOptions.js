8cc0a8e2c5bc98d63ca64842f2a36484c6f7469c
Naasim Shareef <naazim4u@gmail.com>
2019-08-07 17:17:37 +0200

    primary colors updated

--- src/scss/themeOptions.js
+++ src/scss/themeOptions.js
@@ -1,21 +1,23 @@
 export const themeOptions = [
   {
     '--primary': '#001e50',
+    '--secondary': '#ffffff',
     '--bg-primary': '#001f52',
     '--bg-secondary': '#282828',
     '--bg-header': '#ffffff',
+    '--text-primary': '#001e50',
     '--text-alternate': '#666666',
-    '--text-primary': '#ffffff',
     '--text-secondary': '#b3b3b3',
     name: 'light'
   },
   {
     '--primary': '#00b0f0',
+    '--secondary': '#00b0f0',
     '--bg-primary': '#282828',
     '--bg-secondary': '#e8ac90',
     '--bg-header': 'transparent',
+    '--text-primary': '#ffffff',
     '--text-alternate': '#cd626a',
-    '--text-primary': '#fffff2',
     '--text-secondary': '#363b4e',
     name: 'dark'
   }
