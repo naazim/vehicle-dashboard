d265fb2da47a23d61f12bc633123ce4fa839dc90
Naasim Shareef <naazim4u@gmail.com>
2019-08-15 09:48:22 +0200

    Seperate Modal component & styles

---
+++ src/components/Modal/index.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Modal from 'react-responsive-modal';
+import clsx from 'clsx';
+import { ReactComponent as InfoIcon } from '../../assets/info.svg';
+
+export class ModalWrapper extends Component {
+  state = {
+    isModalOpen: false
+  };
+
+  onToggleModal = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  };
+
+  render() {
+    const {
+      vehicleId,
+      vehicleStatus,
+      name,
+      vin,
+      batteryStatus,
+      licensePlateNumber,
+      make,
+      model,
+      vehicleLockStatus,
+      vehicleInteriorTemperature,
+      fleetId,
+      userId,
+      odometer_km
+    } = this.props.data;
+
+    const { isModalOpen } = this.state;
+
+    const batteryStatusValue =
+      (!!batteryStatus && batteryStatus.currentSOC_pct) || 'unknown';
+
+    return (
+      <>
+        <button
+          type="button"
+          className={clsx('btn-icon btn-circle btn-circle__info', {
+            'btn-circle__info--active': isModalOpen
+          })}
+          onClick={this.onToggleModal}
+        >
+          <InfoIcon />
+        </button>
+
+        <Modal
+          open={isModalOpen}
+          onClose={this.onToggleModal}
+          center
+          classNames={{
+            overlay: 'fl-modal__overlay',
+            modal: 'fl-modal',
+            closeIcon: 'fl-modal__close-icon',
+            closeButton: 'fl-modal__close-btn'
+          }}
+          closeIconSvgPath={
+            <>
+              <path d="M.005 26.794L26.875.567l1.12 1.093-26.87 26.227z" />
+              <path d="M1.125.567l26.87 26.227-1.12 1.093L.005 1.66z" />
+            </>
+          }
+        >
+          <h4 className="fl-modal__title">Information</h4>
+          <dl>
+            <dt>name:</dt> <dd>{name}</dd>
+            <dt>license Plate Number:</dt> <dd>{licensePlateNumber}</dd>
+            <dt>vehicleId:</dt> <dd>{vehicleId}</dd>
+            <dt>vehicleStatus:</dt> <dd>{vehicleStatus}</dd>
+            <dt>VIN:</dt> <dd>{vin}</dd>
+            <dt>make:</dt> <dd>{make}</dd>
+            <dt>model:</dt> <dd>{model}</dd>
+            <dt>vehicle Lock Status:</dt> <dd>{vehicleLockStatus}</dd>
+            <dt>vehicleInteriorTemperature:</dt>{' '}
+            <dd>{vehicleInteriorTemperature}</dd>
+            <dt>battery status:</dt> <dd>{batteryStatusValue}</dd>
+            <dt>mileage:</dt> <dd>{odometer_km}</dd>
+            <dt>fleetId:</dt> <dd>{fleetId}</dd>
+            <dt>userId:</dt> <dd>{userId}</dd>
+          </dl>
+        </Modal>
+      </>
+    );
+  }
+}
--- src/components/VehicleFooter/index.js
+++ src/components/VehicleFooter/index.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import Modal from 'react-responsive-modal';
+import { ModalWrapper } from '../Modal';
 import clsx from 'clsx';
 import { Map } from '../Map';
 import { ReactComponent as GpsIcon } from '../../assets/gps.svg';
-import { ReactComponent as InfoIcon } from '../../assets/info.svg';
 
 class VehicleFooter extends Component {
   state = {
-    isModalOpen: false,
     isMapVisible: false,
     location: { lat: '52.5200', lng: '13.4050' }
   };
@@ -31,14 +29,6 @@ class VehicleFooter extends Component {
     }
   }
 
-  onOpenModal = () => {
-    this.setState({ isModalOpen: true });
-  };
-
-  onCloseModal = () => {
-    this.setState({ isModalOpen: false });
-  };
-
   toggleMapVisibility = () => {
     this.setState(() => ({
       isMapVisible: !this.state.isMapVisible
@@ -46,26 +36,7 @@ class VehicleFooter extends Component {
   };
 
   render() {
-    const { isMapVisible, location, isModalOpen } = this.state;
-    const {
-      vehicleId,
-      vehicleStatus,
-      name,
-      vin,
-      batteryStatus,
-      licensePlateNumber,
-      make,
-      model,
-      mileage,
-      vehicleLockStatus,
-      vehicleInteriorTemperature,
-      fleetId,
-      userId,
-      odometer_km
-    } = this.props.vehicleData;
-
-    const batteryStatusValue =
-      (!!batteryStatus && batteryStatus.currentSOC_pct) || 'unknown';
+    const { isMapVisible, location } = this.state;
 
     return (
       <>
@@ -79,52 +50,8 @@ class VehicleFooter extends Component {
           >
             <GpsIcon />
           </button>
-          <button
-            type="button"
-            className={clsx('btn-icon btn-circle btn-circle__map', {
-              'btn-circle__map--active': isMapVisible
-            })}
-            onClick={this.onOpenModal}
-          >
-            <InfoIcon />
-          </button>
+          <ModalWrapper data={this.props.vehicleData} />
         </footer>
-
-        <Modal
-          open={isModalOpen}
-          onClose={this.onCloseModal}
-          center
-          classNames={{
-            overlay: 'fl-modal__overlay',
-            modal: 'fl-modal',
-            closeIcon: 'fl-modal__close-icon',
-            closeButton: 'fl-modal__close-btn'
-          }}
-          closeIconSvgPath={
-            <>
-              <path d="M.005 26.794L26.875.567l1.12 1.093-26.87 26.227z" />
-              <path d="M1.125.567l26.87 26.227-1.12 1.093L.005 1.66z" />
-            </>
-          }
-        >
-          <h4 className="fl-modal__title">Information</h4>
-          <dl>
-            <dt>name:</dt> <dd>{name}</dd>
-            <dt>license Plate Number:</dt> <dd>{licensePlateNumber}</dd>
-            <dt>vehicleId:</dt> <dd>{vehicleId}</dd>
-            <dt>vehicleStatus:</dt> <dd>{vehicleStatus}</dd>
-            <dt>VIN:</dt> <dd>{vin}</dd>
-            <dt>make:</dt> <dd>{make}</dd>
-            <dt>model:</dt> <dd>{model}</dd>
-            <dt>vehicle Lock Status:</dt> <dd>{vehicleLockStatus}</dd>
-            <dt>vehicleInteriorTemperature:</dt>{' '}
-            <dd>{vehicleInteriorTemperature}</dd>
-            <dt>battery status:</dt> <dd>{batteryStatusValue}</dd>
-            <dt>mileage:</dt> <dd>{odometer_km}</dd>
-            <dt>fleetId:</dt> <dd>{fleetId}</dd>
-            <dt>userId:</dt> <dd>{userId}</dd>
-          </dl>
-        </Modal>
 
         <Map
           className={clsx('vehicle-detail__map', {
